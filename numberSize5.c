656cbac7622bc63c20f2ad713177e67b06a87db1
Nakaishi Kousuke <e245729@ie.u-ryukyu.ac.jp>
2025-01-21 19:50:46 +0900

    1st

---
+++ numberSize5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    const int SIZE = 5; 
+    int scores[SIZE] = {0, 60, 70, 100, 90};
+
+    
+    for (int i = 0; i < SIZE - 1; i++) {
+        for (int j = 0; j < SIZE - 1 - i; j++) {
+            if (scores[j] < scores[j + 1]) {
+                
+                int b = scores[j];
+                scores[j] = scores[j + 1];
+                scores[j + 1] = b;
+            }
+        }
+    }
+    printf("results = ");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", scores[i]);
+    }
+    printf("\n");
+
+    printf("scores = ");
+    for (int r = 0; r < SIZE - 1; r++) {
+            for (int k = 0; k < SIZE - 1 ; k++) {
+                if (scores[k] > scores[k + 1]) { 
+                    int temp = scores[k];
+                    scores[k] = scores[k + 1];
+                    scores[k + 1] = temp;
+                }
+            }
+        }
+    for (int r = 0; r < SIZE; r++) {
+    printf("%d ", scores[r]);
+    }
+
+
+    printf("\n");
+
+    return 0;
+}
